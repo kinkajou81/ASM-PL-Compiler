4992f52a1fd7d4d3e95a026e1fe4350bbb7c8aa1
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-04 12:00:43 -0400

    Feat(line_parser): Added new method "listing_virtual_lines" to class line_parser

--- src/line_parser.java
+++ src/line_parser.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+
 public class line_parser {
+    public static ArrayList<String> listify_virtual_lines(String s) {
+        int last_new_line_position = 0;
+        ArrayList<String> virtual_lines = new ArrayList<>();
+
+        int current_char_position = 0;
+        while (current_char_position < s.length()) {
+            if(s.charAt(current_char_position) == com.virtual_new_line_symbol) {
+                virtual_lines.add(s.substring(last_new_line_position, current_char_position));
+                last_new_line_position = current_char_position + 1;
+            }
+            current_char_position++;
+        }
+        if(current_char_position > last_new_line_position) {
+            System.out.printf("Warning: Trailing Code, trailing code will be excluded from the output");
+        }
+        return virtual_lines;
+    }
+
     public static String parse_lines(String s) {
         int i = 0;
         int j;
