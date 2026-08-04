0d8cf91399c703326d9068bd27ea4815e4db3bf0
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-04 11:25:09 -0400

    fix(line_parsing): Fixed class name error
    - Fixed class name error
    - Added temporary return statement null

--- src/line_parsing.java
+++ src/line_parsing.java
@@ -4,15 +4,16 @@ public class line_parsing {
         int j;
         int k = 0;
         int number_of_stages = 0;
-        while(i < semicolon_positions.size()) {
+        while(i < com.semicolon_positions.size()) {
             if(i == 0) {i++; continue;}
-            j = semicolon_positions.get(i);
-            while(j > semicolon_positions.get(i - 1)) {
+            j = com.semicolon_positions.get(i);
+            while(j > com.semicolon_positions.get(i - 1)) {
                 while(k < number_of_stages) {
                     
                 }
             }
             i++;
         }
+        return null; // temporary
     }
 }
