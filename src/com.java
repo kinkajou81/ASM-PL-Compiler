d800a86ef9698d5bcff8a3062cad70a0db92443c
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-05 12:50:59 -0400

    Feat(com.java): Added variable lexer_token_map, variable lexer_token_map_data, and static method load_lexer_token_map_data
    Adding lexer_token_map_data and load_lexer_token_map_data allows for simpler use of lexer_token_map and reduces maintenance

--- src/com.java
+++ src/com.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class com {
     public static String source = new String();
@@ -8,4 +10,21 @@ public class com {
     public static String[][] comment_delimiter_pairs = {{"//", "\n"}, {"/*", "*/"}};
 
     public static ArrayList<Integer> semicolon_positions = new ArrayList<>();
+
+    
+    public static Map<String, types.lexer_token> lexer_token_map = new HashMap<>();
+    public static Object[][] lexer_token_map_data = new Object[][]{ // of the form (String, types.lexer_token)
+        {} // temporarily empty
+    };
+
+    public static void load_lexer_token_map_data() {
+        for(Object[] entry_pair : lexer_token_map_data) {
+            if(entry_pair.length == 2) {
+                lexer_token_map.put((String)entry_pair[0], (types.lexer_token)entry_pair[1]);
+            } else {
+                System.err.printf("ERROR: Malformed lexer_token_map_data");
+                System.exit(-1);
+            }
+        }
+    }
 }
