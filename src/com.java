ff9b5a7454a25df9f18c81d26921f61b9dd7eead
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-05 12:46:20 -0400

    Chore(com.java): Removed unused variables
    Removed due to the method requiring them being removed

--- src/com.java
+++ src/com.java
@@ -3,33 +3,6 @@ import java.util.ArrayList;
 public class com {
     public static String source = new String();
 
-    public static String[][] array_delimiter_pairs = new String[][]{
-        {"(", ")"},
-        {"[", "]"},
-        {"{", "}"},
-        {"//", "\n"},
-        {"\"", "\""},
-        {"'", "'"},
-        {"#", "\n"},
-        {"fn", "endfn"},
-        {"<", ">"},
-        {"tp", "endtp"}
-    };
-    public static boolean[][] shareable_delimiter = new boolean[][]{
-        {false, false},
-        {false, false},
-        {false, false},
-        {false, true},
-        {false, false},
-        {false, false},
-        {false, true},
-        {false, false},
-        {false, false},
-        {false, false}
-
-    };
-    public static boolean[] persistent_delimiters = new boolean[]{false, false, true, false, false, false, false, true, false, true};
-    public static boolean[] string_pairs = new boolean[]{false, false, false, true, true, true, true, false, false, false};
     public static char virtual_new_line_symbol = ';';
     public static char[] string_delimiters = {'"', '\''};
     public static String[][] comment_delimiter_pairs = {{"//", "\n"}, {"/*", "*/"}};
