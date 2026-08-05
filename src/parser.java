79f5f500802386c7ac045b477f0ff80ea6839bc8
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-05 12:25:56 -0400

    Chore(parser.java): Removed incomplete static method parse_lines
    Removed due to change in parsing logic

--- src/parser.java
+++ src/parser.java
@@ -100,22 +100,4 @@ public class parser {
 
         return output_builder.toString();
     }
-
-    public static String parse_lines(String s) {
-        int i = 0;
-        int j;
-        int k = 0;
-        int number_of_stages = 0;
-        while(i < com.semicolon_positions.size()) {
-            if(i == 0) {i++; continue;}
-            j = com.semicolon_positions.get(i);
-            while(j > com.semicolon_positions.get(i - 1)) {
-                while(k < number_of_stages) {
-                    
-                }
-            }
-            i++;
-        }
-        return null; // temporary
-    }
 }
