6513f5fc3375af6a02bdfcb6d0b2dd8c2bc241ac
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-05 15:30:37 -0400

    Feat(com.java): Added entries for variable lexer_token_map_data

--- src/com.java
+++ src/com.java
@@ -14,7 +14,47 @@ public class com {
     
     public static Map<String, types.lexer_token> lexer_token_map = new HashMap<>();
     public static Object[][] lexer_token_map_data = new Object[][]{ // of the form (String, types.lexer_token)
-        {} // temporarily empty
+        {"#", types.lexer_token.DIRECTIVE},
+        {"!", types.lexer_token.NOT},
+        {".<-", types.lexer_token.UNASSIGNMENT},
+        {":\n", types.lexer_token.LABEL},
+        {"<-", types.lexer_token.ASSIGNMENT},
+        {"~", types.lexer_token.WHILE},
+        {"==", types.lexer_token.EQUAL_TO},
+        {"<", types.lexer_token.LESS_THAN},
+        {">", types.lexer_token.GREATER_THAN},
+        {"<=", types.lexer_token.LESS_THAN_EQUAL_TO},
+        {">=", types.lexer_token.GREATER_THAN_EQUAL_TO},
+        {"!=", types.lexer_token.NOT_EQUAL_TO},
+        {"<<", types.lexer_token.BITSHIFT_LEFT},
+        {">>", types.lexer_token.BITSHIFT_RIGHT},
+        {"^<", types.lexer_token.BITROTATE_LEFT},
+        {"^>", types.lexer_token.BITROTATE_RIGHT},
+        {"/", types.lexer_token.DIVIDE},
+        {".", types.lexer_token.DOT},
+        {"->", types.lexer_token.STRUCTURE_DEREFERNCE},
+        {"::", types.lexer_token.NAMESPACE_RESOLUTION},
+        {",", types.lexer_token.LIST_SEPARATOR},
+        {"%", types.lexer_token.MODULO},
+        {"?:", types.lexer_token.ELVIS},
+        {"&", types.lexer_token.AMPERSAND},
+        {"*", types.lexer_token.ASTERISK},
+        {"+", types.lexer_token.PLUS},
+        {"-", types.lexer_token.MINUS},
+        {"^", types.lexer_token.CARET},
+        {"|", types.lexer_token.VERTICAL_LINE},
+        {"?", types.lexer_token.TERNARY_PART1},
+        {"fn", types.lexer_token.FUNCTION_DEF},
+        {"endfn", types.lexer_token.FUNCTION_END},
+        {"(", types.lexer_token.PAREN_LEFT},
+        {")", types.lexer_token.PAREN_RIGHT},
+        {"[", types.lexer_token.SQUARE_BRACKET_LEFT},
+        {"]", types.lexer_token.SQUARE_BRACKET_RIGHT},
+        {"\"", types.lexer_token.DOUBLE_QUOTE},
+        {"'", types.lexer_token.QUOTE},
+        {"{", types.lexer_token.CURLY_LEFT},
+        {"}", types.lexer_token.CURLY_RIGHT},
+        {":", types.lexer_token.COLON}
     };
 
     public static void load_lexer_token_map_data() {
