27020419b39413641c1cf563d743d3ef0601cda1
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-13 21:24:07 -0400

    Fix(main.java): Fixed leading and trailing whitespace removal

--- src/main.java
+++ src/main.java
@@ -3,10 +3,11 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static String parse(String s) {
-        com.source = s.stripIndent();
+        com.source = s.lines().map(String::strip).collect(Collectors.joining("\n"));
         com.is_string = parser.find_strings(com.source);
         com.source = parser.remove_comments(com.source, com.is_string);
         com.is_string = parser.find_strings(com.source);
