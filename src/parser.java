3a941628be7a93b2c9b45eeb77efa31a7ee61010
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-08-20 16:33:45 -0400

    Refactor(parser): Removed unused method listify_virtual_lines

--- src/parser.java
+++ src/parser.java
@@ -2,24 +2,6 @@ import java.util.ArrayList;
 import java.util.BitSet;
 
 public class parser {
-    public static ArrayList<String> listify_virtual_lines(String s) {
-        int last_new_line_position = 0;
-        ArrayList<String> virtual_lines = new ArrayList<>();
-
-        int current_char_position = 0;
-        while (current_char_position < s.length()) {
-            if(s.charAt(current_char_position) == com.virtual_new_line_symbol) {
-                virtual_lines.add(s.substring(last_new_line_position, current_char_position));
-                last_new_line_position = current_char_position + 1;
-            }
-            current_char_position++;
-        }
-        if(current_char_position > last_new_line_position) {
-            virtual_lines.add(s.substring(last_new_line_position, current_char_position));
-        }
-        return virtual_lines;
-    }
-
     public static BitSet find_strings(String s) {
         BitSet is_string = new BitSet();
 
